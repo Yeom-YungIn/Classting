e59bc74dbb3a7730f5716ee8e017a0b72e00764e
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-13 17:35:12 +0900

    feat: PageService createPage 구현

--- src/school/service/page.service.ts
+++ src/school/service/page.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Page} from "../entity/page.entity";
+import {Repository} from "typeorm";
+import {PageDto} from "../dto/page.dto";
+import {validate} from "class-validator";
 
 @Injectable()
 export class PageService {
+    constructor(
+        @InjectRepository(Page)
+        private readonly pageRepository: Repository<Page>
+    ) {
+    }
+
+    async createPage(newPageData: {schoolName: string, location: string}): Promise<Page> {
+        const newPage: Page = this.pageRepository.create({
+            ...newPageData,
+        });
+
+        return await this.pageRepository.save(newPage);
+    }
 }
