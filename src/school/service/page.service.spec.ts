73778e2b8bc4b1248b659fefc45a4b3aa61e274f
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-13 16:21:15 +0900

    test: 페이지 생성 메서드(createPage) 테스트 케이스 추가

--- src/school/service/page.service.spec.ts
+++ src/school/service/page.service.spec.ts
@@ -3,16 +3,16 @@ import {PageService} from "./page.service";
 import {Repository} from "typeorm";
 import {Page} from "../entity/page.entity";
 import {getRepositoryToken} from "@nestjs/typeorm";
-
+import {PageDto} from "../dto/page.dto";
 
 
 describe('PageService', () => {
   let service: PageService;
   let repository: Repository<Page>;
-
+  let newPage: PageDto;
 
   beforeEach(async () => {
-
+    newPage = { schoolName: 'schoolName', location: 'location'};
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -32,4 +32,27 @@ describe('PageService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('PageService.createPage', () => {
+    beforeEach(() => {
+      repository.create = jest.fn();
+      repository.save = jest.fn().mockResolvedValueOnce(newPage);
+    })
+
+    it('createPage should be a defined & function', () => {
+      expect(service.createPage).toBeDefined();
+      expect(typeof service.createPage).toBe('function');
+    });
+
+    it('createPage should have called repository.{save & create} with newPage', async () => {
+      await service.createPage(newPage);
+      expect(repository.create).toHaveBeenCalledWith(newPage);
+      expect(repository.save).toBeCalled();
+    })
+
+    it('createPage should return the created page', async () => {
+      const result: Page = await service.createPage(newPage)
+      expect(result.schoolName).toEqual(newPage.schoolName);
+      expect(result.location).toEqual(newPage.location);
+    })
+  });
 });
