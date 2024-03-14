aab4d2fbed1c0992035780b373238f54e091b45c
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-14 15:30:47 +0900

    feat: school module typeOrmModule import & provider 추가
    - page, news, subscribe entity import
    - NewsService, PageService, Subscribe provider add

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { SchoolController } from './controller/school.controller';
-import { SchoolService } from './service/school.service';
+import { SchoolPageController } from './controller/school-page.controller';
+import { NewsService } from './service/news.service';
+import {PageService} from "./service/page.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Page} from "./entity/page.entity";
+import {News} from "./entity/news.entity";
+import {Subscribe} from "./entity/subscribe.entity";
 
 @Module({
-  controllers: [SchoolController],
-  providers: [SchoolService]
+  imports: [TypeOrmModule.forFeature([Page, News, Subscribe])],
+  controllers: [SchoolPageController],
+  providers: [
+      NewsService,
+      PageService,
+      Subscribe,
+  ]
 })
 export class SchoolModule {}
