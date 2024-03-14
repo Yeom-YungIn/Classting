eefeedd9e96c8fa269268551c16a7ed627e006e3
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-14 15:34:25 +0900

    feat: NewsService method 구현
    - 생성, 목록 조회, ID 조회,수정, 삭제 기능 구현[createNews, getNewsList, getNewsById, updateNews, deleteNews]

--- src/school/service/news.service.ts
+++ src/school/service/news.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {News} from "../entity/news.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+    constructor(
+        @InjectRepository(News)
+        private readonly newsRepository: Repository<News>
+    ) {
+    }
+
+    async createNews(newsObj: {pageId: number, content: string}): Promise<News> {
+        const newNews = this.newsRepository.create({
+            ...newsObj
+        });
+
+        return await this.newsRepository.save(newNews);
+    }
+
+    async getNewsList(pageId: number) {
+        const where = {
+            pageId,
+            order: {createAt: 'DESC'},
+            skip: 0,
+            take: 10,
+        };
+
+        const option = {
+            where,
+            order: {createAt: 'DESC'},
+            skip: 0,
+            take: 10,
+        }
+
+        const found = await this.newsRepository.findBy(where)
+        return found;
+    }
+
+    async getNewsById(newsId: number) {
+        const foundNews: News = await this.newsRepository.findOneBy({newsId});
+        if (!foundNews) {
+            throw new NotFoundException();
+        }
+        return foundNews;
+    }
+
+    async updateNews(newsId: number, content: string) {
+        const foundNews: News = await this.getNewsById(newsId);
+
+        const updatedNews = await this.newsRepository.update({newsId: foundNews.newsId}, {content});
+
+        if (updatedNews.affected) {
+            return { result: "success", ...foundNews }
+        }
+    }
+
+    async deleteNews(newsId: number) {
+        const foundNews: News = await this.getNewsById(newsId);
+
+        const deletedNews = await this.newsRepository.delete({newsId: foundNews.newsId});
+        if (deletedNews.affected) {
+            return { result: "success", ...foundNews }
+        }
+    }
+}
