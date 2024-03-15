67bda0f49aec7741156aa3336ea3bb01c5c58d4c
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-03-15 15:21:07 +0900

    feat: PageService findPageById 구현

--- src/school/service/page.service.ts
+++ src/school/service/page.service.ts
@@ -11,7 +11,11 @@ export class PageService {
     ) {
     }
 
-    async createPage(newPageData: {schoolName: string, location: string}): Promise<Page> {
+    async findPageById(pageId: number): Promise<Page> {
+        return await this.pageRepository.findOneBy({pageId});
+    }
+
+    async createPage(schoolName: string, location: string): Promise<Page> {
         const newPage: Page = this.pageRepository.create({
             schoolName,
             location,
